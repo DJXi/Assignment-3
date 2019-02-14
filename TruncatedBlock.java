b535af943f134d70a98ffb8ee021f5d5698ad075
pupilxx <47097477+pupilxx@users.noreply.github.com>
2019-02-13 19:52:36 -0600

    documentations

--- TruncatedBlock.java
+++ TruncatedBlock.java
@@ -1,9 +1,10 @@
 package lab.polymorphism;
 
 public class TruncatedBlock implements TextBlock {
+  // Fields
   private TextBlock truncatedTb;
   private int width;
- 
+  // Constructor
   public TruncatedBlock(TextBlock tb, int width) throws Exception{
     if((width < 0) || (width > tb.width())) {
       throw new Exception("Invalid width!");
@@ -12,19 +13,19 @@ public class TruncatedBlock implements TextBlock {
       this.truncatedTb = tb;
     }
   }
- 
+  // method of getting the height
   public int height() {
     return this.truncatedTb.height();
   }
- 
+  // method of getting the width
   public int width() {
     return this.width;
   }
- 
+  // method of returning each row of the textblock
   public String row(int i) throws Exception {
     String row = this.truncatedTb.row(i);
     row = row.substring(0, this.width);
     return row;
   }
-}
+}// Class for TruncatedBlock
 
