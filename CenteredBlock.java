35a6699f8523ac17243e2207085da95dbfcf1178
pupilxx <47097477+pupilxx@users.noreply.github.com>
2019-02-13 19:51:09 -0600

    documentations

--- CenteredBlock.java
+++ CenteredBlock.java
@@ -1,10 +1,10 @@
 package lab.polymorphism;
 
 public class CenteredBlock implements TextBlock {
-
+  // Field
   TextBlock block;
   int width;
-
+  // Constructor
   public CenteredBlock(TextBlock block, int width) throws IllegalArgumentException {
     if (width < block.width()) {
       throw new IllegalArgumentException("Width is too small");
@@ -12,7 +12,7 @@ public class CenteredBlock implements TextBlock {
     this.block = block;
     this.width = width;
   }
-
+  // method of returning each row of the textblock
   public String row(int i) throws Exception {
       String row = this.block.row(i);
       while (row.charAt(row.length() - 1) == ' ') {
@@ -24,17 +24,12 @@ public class CenteredBlock implements TextBlock {
       return TBUtils.spaces((this.width - row.length()) / 2) + row
           + TBUtils.spaces(this.width - row.length() -(this.width - row.length()) / 2);
     }
-
- 
-
+  // method of getting the height
   public int height() {
     return this.block.height();
   }
-
-
+  // method of getting the width
   public int width() {
     return this.width;
   }
-
-
-}
+} // Class CenteredBlock
