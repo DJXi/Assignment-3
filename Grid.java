a831618b5c1c2a6bca98280c8a10007d8ab7f347
pupilxx <47097477+pupilxx@users.noreply.github.com>
2019-02-13 19:54:27 -0600

    documentations

--- Grid.java
+++ Grid.java
@@ -1,28 +1,27 @@
 package lab.polymorphism;
+// import packages
 import java.io.PrintWriter;
 
 public class Grid implements TextBlock {
-
-
-
+  // Fields
   int width;
   int height;
   char ch;
-  
+  // Constructor
   public Grid (int width, int height, char ch) {
     this.width = width;
     this.height = height + 2;
     this.ch = ch;
   }
-  
+  // the method of getting height
   public int height() {
     return this.height; 
   }
-  
+  // method of getting width
   public int width() {
     return this.width;
   }
-  
+  // method of returning each row of the textblock
   public String row(int i) throws Exception {
     // The top and bottom of the box
     String value = Character.toString(ch);
@@ -38,7 +37,7 @@ public class Grid implements TextBlock {
       throw new Exception("Invalid row " + i);
     }
   }
-  
+  // testing
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
     TextBlock grid = new Grid(4, 6, '8');
